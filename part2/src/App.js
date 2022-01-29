cbd708526d0d3cc3486ad7b7be0593f886187e7b
gonzaloVG <gonzaloverdugog@gmail.com>
2022-01-29 19:05:55 +0100

    part2 20220129

---
+++ part2/src/App.js
@@ -0,0 +1,42 @@
+import Note from "./components/Note";
+import React, { useState } from "react";
+
+export default function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('')
+  };
+
+  const handleNoteChange = (event) => {
+    console.log("event.target.value", event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note.content} />
+        ))}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}></input>
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+}

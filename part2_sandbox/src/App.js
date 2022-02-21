7b4bc926da6636fb9bae9f0bb400e1c39b361285
gonzaloVG <gonzaloverdugog@gmail.com>
2022-02-21 20:52:18 +0100

    part3 back

--- part2_sandbox/src/App.js
+++ part2_sandbox/src/App.js
@@ -1,11 +1,25 @@
 import Note from "./components/Note";
+import Notification from "./components/Notification";
 import React, { useState, useEffect } from "react";
 import noteService from "./services/notes";
+import Footer from "./components/Footer";
+import "./index.css";
+
+const errorStyle = {
+    color: "red",
+};
+
+const correctStyle = {
+    color: "green",
+};
 
 export default function App(props) {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState("");
     const [showAll, setShowAll] = useState(true);
+    const [notificationMessage, setNotificationMessage] =
+        useState("gonso site");
+    const [notificationStyle, setNotificationStyle] = useState(correctStyle);
 
     const toggleImportanceOf = (id) => {
         const note = notes.find((n) => n.id === id);
@@ -19,16 +33,26 @@ export default function App(props) {
                 );
             })
             .catch((error) => {
-                alert(
-                    `the note '${note.content}' was already deleted from server`
+                setNotificationStyle(errorStyle);
+                setNotificationMessage(
+                    `Note '${note.content}' was already removed from server`
                 );
+                setTimeout(() => {
+                    setNotificationMessage(null);
+                }, 5000);
                 setNotes(notes.filter((n) => n.id !== id));
             });
     };
 
     const getNotes = () => {
+        const nonExisting = {
+            id: 10000,
+            content: "This note is not saved to server",
+            date: "2019-05-30T17:30:31.098Z",
+            important: true,
+        };
         noteService.getAll().then((initialNotes) => {
-            setNotes(initialNotes);
+            setNotes(initialNotes.concat(nonExisting));
         });
     };
     const handleNoteChange = (event) => {
@@ -45,10 +69,30 @@ export default function App(props) {
             important: Math.random() < 0.5,
         };
 
-        noteService.create(noteObject).then((returnedNote) => {
-            setNotes(notes.concat(returnedNote));
-            setNewNote("");
-        });
+        noteService
+            .create(noteObject)
+            .then((returnedNote) => {
+                setNotes([...notes.concat(returnedNote)]);
+                setNewNote("");
+            })
+            .finally(() => {
+                setNotificationStyle(correctStyle);
+                setNotificationMessage("success");
+            });
+    };
+
+    const deleteNote = (id) => {
+        noteService
+            .deleteNote(id)
+            .then(setNotes([...notes.filter((note) => note.id !== id)]))
+            .catch((error) => {
+                setNotificationStyle(errorStyle);
+                setNotificationMessage(`Note was already removed from server`);
+                setTimeout(() => {
+                    setNotificationMessage(null);
+                }, 5000);
+                setNotes(notes.filter((n) => n.id !== id));
+            });
     };
 
     const noteToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -57,6 +101,10 @@ export default function App(props) {
     return (
         <div>
             <h1>Notes</h1>
+            <Notification
+                style={notificationStyle}
+                message={notificationMessage}
+            />
             <button onClick={() => setShowAll(!showAll)}>
                 show {showAll ? "important" : "important"}
             </button>
@@ -66,6 +114,7 @@ export default function App(props) {
                         key={note.id}
                         note={note}
                         toogleImportance={() => toggleImportanceOf(note.id)}
+                        onDeleteClick={() => deleteNote(note.id)}
                     />
                 ))}
             </ul>
@@ -74,6 +123,7 @@ export default function App(props) {
                 <input value={newNote} onChange={handleNoteChange}></input>
                 <button type="submit">save</button>
             </form>
+            <Footer></Footer>
         </div>
     );
 }
--- part2_sandbox/src/components/Note.js
+++ part2_sandbox/src/components/Note.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Note = ({ note, toogleImportance }) => {
+const Note = ({ note, toogleImportance, onDeleteClick }) => {
     const label = note.important ? "make not important" : "make important";
 
     return (
-        <li>
+        <li className="note">
             {note.content}
             <button onClick={toogleImportance}>{label}</button>
+            <button onClick={onDeleteClick}>Delete</button>
         </li>
     );
 };
